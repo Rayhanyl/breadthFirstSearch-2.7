c3a240fdd6538d79d7d6ffa41395ad30f03aa523
ch-egli <chrsitian.egli@gmx.net>
2019-10-27 01:18:16 +0200

    some enhancements

--- breitensuche-1.py
+++ breitensuche-1.py
@@ -15,37 +15,42 @@ def breitensuche(graph, start_knoten):
     return besuchte_knoten
 
 
-def breitensucheAlpha(graph, start_knoten):
-    warteschlange = [start_knoten]
-    besuchte_knoten = [start_knoten]
-    while warteschlange:
-        aktueller_knoten = warteschlange.pop(0)
-        for nachbar in nachbarn_von(aktueller_knoten, graph):
-            warteschlange.append(nachbar)
-            besuchte_knoten.append(nachbar)
-    return besuchte_knoten
-
-
 def pfad(eltern_dict, ziel_knoten):
     result = [ziel_knoten];
     vater = eltern_dict[ziel_knoten]
-    while vater != None:
+    while vater is not None:
         result.append(vater)
         vater = eltern_dict[vater]
     result.reverse()
     return result
 
 
+def ist_weg_vorhanden(graph, start_knoten, ziel_knoten):
+    if start_knoten == ziel_knoten:
+        return True
+    warteschlange = [start_knoten]
+    besuchte_knoten = [start_knoten]
+    while warteschlange:
+        aktueller_knoten = warteschlange.pop(0)
+        for nachbar in nachbarn_von(aktueller_knoten, graph):
+            if nachbar == ziel_knoten:
+                return True
+            elif nachbar not in besuchte_knoten:
+                warteschlange.append(nachbar)
+                besuchte_knoten.append(nachbar)
+    return False
+
+
 def kuerzester_weg(graph, start_knoten, ziel_knoten):
-    if (start_knoten == ziel_knoten):
+    if start_knoten == ziel_knoten:
         return [start_knoten]
     warteschlange = [start_knoten]
     besuchte_knoten = [start_knoten]
     eltern_dict = {start_knoten: None}
     while warteschlange:
         aktueller_knoten = warteschlange.pop(0)
-        for nachbar in graph[aktueller_knoten]:
-            if (nachbar == ziel_knoten):
+        for nachbar in nachbarn_von(aktueller_knoten, graph):
+            if nachbar == ziel_knoten:
                 besuchte_knoten.append(nachbar)
                 eltern_dict[nachbar] = aktueller_knoten
                 return pfad(eltern_dict, ziel_knoten)
@@ -57,26 +62,6 @@ def kuerzester_weg(graph, start_knoten, ziel_knoten):
 
 
 graph_1 = {
-    'A': ['B', 'C'],
-    'B': ['A', 'D', 'E'],
-    'C': ['A', 'D'],
-    'D': ['B', 'C', 'E'],
-    'E': ['B', 'D']
-}
-
-graph_2 = {
-    'A': ['B', 'C'],
-    'B': ['A', 'C', 'D', 'E', 'F'],
-    'C': ['A', 'B', 'E'],
-    'D': ['B', 'F', 'H'],
-    'E': ['B', 'C', 'G'],
-    'F': ['B', 'D', 'G', 'H', 'I'],
-    'G': ['E', 'F', 'I'],
-    'H': ['D', 'F'],
-    'I': ['F', 'G']
-}
-
-graph_3 = {
     'A': ['B', 'D', 'F'],
     'B': ['A', 'C', 'E'],
     'C': ['B', 'E', 'I'],
@@ -88,7 +73,7 @@ graph_3 = {
     'I': ['C', 'E', 'H']
 }
 
-graph_4 = {
+graph_2 = {
     'A': ['B'],
     'B': ['A', 'C', 'E'],
     'C': ['B', 'D', 'F'],
@@ -103,21 +88,24 @@ graph_4 = {
     'L': ['K']
 }
 
-print "Breitensuche graph_1: ", breitensuche(graph_1, 'A')
-print "Breitensuche graph_2: ", breitensuche(graph_2, 'A')
-print "Breitensuche graph_3: ", breitensuche(graph_3, 'A')
-print "Breitensuche graph_4: ", breitensuche(graph_4, 'D')
+print "Breitensuche in graph_1: ", breitensuche(graph_1, 'A')  # A B D F C E G I H
+print "Breitensuche in graph_2: ", breitensuche(graph_2, 'D')  # D C I B F H K A E G J L
 print ""
-print "Kuerzester Weg graph_3"
-print "  von A nach D: ", kuerzester_weg(graph_3, 'A', 'D')  # A D
-print "  von C nach F: ", kuerzester_weg(graph_3, 'C', 'F')  # C B A F
-print "  von A nach H: ", kuerzester_weg(graph_3, 'A', 'H')  # A B D H
-print "  von A nach Z: ", kuerzester_weg(graph_4, 'A', 'Z')  # A
+print "Wegsuche in graph_1"
+print "  von A nach D: ", ist_weg_vorhanden(graph_1, 'A', 'D')  # True
+print "  von A nach A: ", ist_weg_vorhanden(graph_1, 'A', 'A')  # True
+print "  von A nach Z: ", ist_weg_vorhanden(graph_1, 'A', 'Z')  # False
 print ""
-print "Kuerzester Weg graph_4"
-print "  von D nach J: ", kuerzester_weg(graph_4, 'D', 'J')  # D I K J
-print "  von K nach F: ", kuerzester_weg(graph_4, 'K', 'F')  # K I D C F
-print "  von E nach G: ", kuerzester_weg(graph_4, 'E', 'G')  # E B C D I H G
-print "  von C nach C: ", kuerzester_weg(graph_4, 'C', 'C')  # C
+print "Kuerzester Weg in graph_1"
+print "  von A nach D: ", kuerzester_weg(graph_1, 'A', 'D')  # A D
+print "  von C nach F: ", kuerzester_weg(graph_1, 'C', 'F')  # C B A F
+print "  von A nach H: ", kuerzester_weg(graph_1, 'A', 'H')  # A B D H
+print "  von A nach Z: ", kuerzester_weg(graph_2, 'A', 'Z')  # A
+print ""
+print "Kuerzester Weg in graph_2"
+print "  von D nach J: ", kuerzester_weg(graph_2, 'D', 'J')  # D I K J
+print "  von K nach F: ", kuerzester_weg(graph_2, 'K', 'F')  # K I D C F
+print "  von E nach G: ", kuerzester_weg(graph_2, 'E', 'G')  # E B C D I H G
+print "  von C nach C: ", kuerzester_weg(graph_2, 'C', 'C')  # C
 
-
+print "Nachbarn von B: ", graph_2['B']
